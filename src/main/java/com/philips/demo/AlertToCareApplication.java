98324e059d3390c64262d85491c56d9fa2d2fd89
Ravikumar Tiwari <ravitiwari201@gmail.com>
2020-10-13 11:40:53 +0530

    Uploading project with basic file structure

---
+++ src/main/java/com/philips/demo/AlertToCareApplication.java
@@ -0,0 +1,13 @@
+package com.philips.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertToCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertToCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/philips/demo/AlertToCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philips.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertToCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

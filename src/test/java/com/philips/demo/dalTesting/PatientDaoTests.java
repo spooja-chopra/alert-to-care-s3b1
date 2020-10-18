8398fb9632b9bc47ec386b7b2ff67b6704d0c119
Ravikumar Tiwari <ravitiwari201@gmail.com>
2020-10-18 13:26:47 +0530

    Dao test cases for patients done

--- src/test/java/com/philips/demo/dalTesting/PatientDaoTests.java
+++ src/test/java/com/philips/demo/dalTesting/PatientDaoTests.java
@@ -3,6 +3,9 @@ package com.philips.demo.dalTesting;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,33 +33,87 @@ public class PatientDaoTests {
 	
 	@Test
 	public void testAddPatient() {
-		Patient newPatient = getPatient();
+		Patient newPatient = getFirstPatient();
+		
+		Patient addInDb = patientDao.addPatient(newPatient, newPatient.getBed().getBedId());
+		Patient getFromDb = entityManager.find(Patient.class, newPatient.getPatient_Id());
+		
+		assertThat(getFromDb.getAge()).isEqualTo(23);
+		assertThat(getFromDb.getName()).isEqualTo("Ravikumar Tiwari");
+		assertThat(getFromDb.getPhone_Number()).isEqualTo("1258795367");
+		assertThat(getFromDb).isEqualTo(addInDb);
+	}
+	
+	
+	@Test
+	public void testFindPatient() {
+		Patient newPatient = getFirstPatient();
+		
 		Patient savedInDb = entityManager.persist(newPatient);
-		
 		Patient getFromDb = patientDao.findPatient(savedInDb.getPatient_Id());
-	
-		assertThat(savedInDb.getAge()).isEqualTo(23);
-		assertThat(savedInDb.getName()).isEqualTo("Ravikumar Tiwari");
-		assertThat(savedInDb.getPhone_Number()).isEqualTo("1258795367");
-		assertThat(getFromDb).isEqualTo(savedInDb);
 		
+		assertThat(getFromDb).isEqualTo(savedInDb);
 	}
 	
-	private Patient getPatient() {
-		Patient patient = new Patient();
-		patient.setName("Ravikumar Tiwari");
-		patient.setAge(23);
-		patient.setPhone_Number("1258795367");
-		patient.setBed(getBed());
-		return patient;
+	@Test
+	public void testFindAllPatient() {
+		List<Patient> allPatients = new ArrayList<>();
+		
+		Patient firstPatient = getFirstPatient();
+		Patient secondPatient = getSecondPatient();
+		
+		Patient saveFirstPatient = entityManager.persist(firstPatient);
+		Patient saveSecondPatient = entityManager.persist(secondPatient);
+		
+		allPatients = patientDao.findAll();
+		
+		assertThat(allPatients.get(0)).isEqualTo(saveFirstPatient);
+		assertThat(allPatients.get(1)).isEqualTo(saveSecondPatient);
+		assertThat(allPatients.get(0).getName()).isEqualTo("Ravikumar Tiwari");
+		assertThat(allPatients.get(1).getName()).isEqualTo("Ram Vaishnav");
 	}
 	
-	private Bed getBed() {
-		Bed myBed = new Bed();
-		myBed.setBedSerialNumber(101);
-		myBed.setBedAvailability(true);
-		myBed.seticuNumber(1);
-		entityManager.persist(myBed);
-		return myBed;
+	private Patient getFirstPatient() {
+		Patient patient1 = new Patient();
+		
+		patient1.setName("Ravikumar Tiwari");
+		patient1.setAge(23);
+		patient1.setPhone_Number("1258795367");
+		patient1.setBed(getBedForFirstPatient());
+		
+		return patient1;
+	}
+	
+	private Patient getSecondPatient() {
+		Patient patient2 = new Patient();
+		
+		patient2.setName("Ram Vaishnav");
+		patient2.setAge(25);
+		patient2.setPhone_Number("4378382847");
+		patient2.setBed(getBedForSecondPatient());
+		
+		return patient2;
+	}
+	
+	private Bed getBedForFirstPatient() {
+		Bed bed1 = new Bed();
+		
+		bed1.setBedSerialNumber(101);
+		bed1.setBedAvailability(true);
+		bed1.seticuNumber(1);
+		entityManager.persist(bed1);
+		
+		return bed1;
+	}
+	
+	private Bed getBedForSecondPatient() {
+		Bed bed2 = new Bed();
+		
+		bed2.setBedSerialNumber(107);
+		bed2.setBedAvailability(true);
+		bed2.seticuNumber(2);
+		entityManager.persist(bed2);
+		
+		return bed2;
 	}
 }
